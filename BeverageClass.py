d69f1b6100cb611022732ed1c3b49a14089dfeb6
KuskIV <mkusk18@student.aau.dk>
2020-01-12 15:41:01 +0100

    calc alc

--- BeverageClass.py
+++ BeverageClass.py
@@ -1,8 +1,9 @@
 from CalcAlcoholClass import CalcAlcohol
 
-class Beverages():
+class Beverage():
     def __init__(self, _amount, _percent_alcohol):
         self.amount = _amount
         self.percent_alcohol = _percent_alcohol
         self.gram_of_alcohol = CalcAlcohol.get_gram_of_alcohol(self.amount, self.percent_alcohol)
         self.oz_of_alc = CalcAlcohol.get_oz_of_alcohol(self.amount, self.percent_alcohol)
+        self.miligram_alc_pr_hundred_ml = CalcAlcohol.miligram_alc_pr_hundred_ml(self.amount, self.percent_alcohol)
--- CalcAlcoholClass.py
+++ CalcAlcoholClass.py
@@ -14,14 +14,14 @@ class CalcAlcohol:
 
     @staticmethod
     def get_gram_of_alcohol(amount, percent_alcohol):
-        return ml_to_oz(amount) * 29.5735 * (percent_alcohol / 100) * 0.789
+        return CalcAlcohol.ml_to_oz(amount) * 29.5735 * (percent_alcohol / 100) * 0.789
 
     @staticmethod
     def get_oz_of_alcohol(amount, percent_alcohol):
-        result = get_gram_of_alcohol(amount, percent_alcohol)
-        return gram_to_oz(result)
+        result = CalcAlcohol.get_gram_of_alcohol(amount, percent_alcohol)
+        return CalcAlcohol.gram_to_oz(result)
         
     @staticmethod
     def miligram_alc_pr_hundred_ml(amount, percent_alcohol):
-        result = get_gram_of_alcohol(amount, percent_alcohol)
-        return gram_to_miligram(result)
+        result = CalcAlcohol.get_gram_of_alcohol(amount, percent_alcohol)
+        return CalcAlcohol.gram_to_miligram(result)
