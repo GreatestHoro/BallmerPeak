4d022358d6ad344ae9c1dc06e02f131213504a2d
Jeppe <jeppe@bandsholm.com>
2020-01-13 13:51:49 +0100

    works

--- SessionClass.py
+++ SessionClass.py
@@ -4,7 +4,10 @@ from BeverageClass import Beverage
 class Session():
     beverages = []
     beverage_time = {}
-    duration = 0
+    duration_hour = 0
+    duration_minut = 0
+    duration_sec = 0
+
 
     def __init__(self):
         self.start = datetime.now()
@@ -13,8 +16,19 @@ class Session():
         cls.beverages.append(beverage)
         cls.beverage_time[beverage] = datetime.now()
         cls.End = datetime.now()
+    
 
     @property
-    def duration(cls):
+    def duration_hour(cls):
         dif = datetime.now() - cls.start
-        return round(((dif.total_seconds()/60)/60),5)
+        return round((cls.duration_minut/60),5)
+
+    @property
+    def duration_minut(cls):
+        dif = datetime.now() - cls.start
+        return int(round(cls.duration_sec/60,0))
+        
+    @property
+    def duration_sec(cls):
+        dif = datetime.now() - cls.start
+        return int(round(dif.total_seconds(),0))
