97fc28df3f9ed253d88c7a2d2e9d78903d833779
Jeppe <jeppe@bandsholm.com>
2020-01-15 16:13:20 +0100

    hi

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ import time
 from BeverageClass import Beverage
 from GraphDisplayer import GraphDisplayer as GDisplay
 
+dict = {1:"one",2:"two",3:"three"} #dictionary
+print(dict.get(1))
+
 bac = BAC()
 name = "John Doe"
 age = 42
@@ -11,19 +14,19 @@ weight = 100
 height = 180
 gender = ps.Gender.male
 
-b = Beverage(33, 4.5)
+#b = Beverage()
 p = ps.Person(name, gender.name, age, weight, height)
 
 print(bac.calc_body_water(p), "Calc body water")
 print(bac.female_body_blood(p), "Female body blood")
 print(p.name, "is a", p.gender)
 
-b = Beverage(33, 4.5)
+#b = Beverage(33, 4.5)
 print(len(p.session.beverages))
-p.session.add_beverage(b)
+#p.session.add_beverage(b)
 print(len(p.session.beverages))
-time.sleep(10)
+time.sleep(1)
 print(p.session.duration_sec)
 
 GDisplay(p)
-print(b.gram_of_alcohol, "gram of alcohol in a beverage consisting of", b.amount, "ml and ", b.percent_alcohol, "percent alcohol")
+#print(b.gram_of_alcohol, "gram of alcohol in a beverage consisting of", b.amount, "ml and ", b.percent_alcohol, "percent alcohol")
